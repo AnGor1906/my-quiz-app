ce54461d51db510f893d4ec6a492f518fb241f61
Ania Gorbik <angor1906@gmail.com>
2021-07-01 15:28:14 +0300

    first commit

--- src/Cabinet.js
+++ src/Cabinet.js
@@ -14,9 +14,10 @@ class Cabinet extends Component {
     signInFlow: "popup",
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+       firebase.auth.EmailAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+     
     ],
     callbacks: {
       signInSuccess: () => false
